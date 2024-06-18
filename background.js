e4abe9f88b9950267a2385ab3e8467b496772ee3
Remy Sharp <remy@remysharp.com>
2024-06-18 10:35:59 +0100

    drop around 100ms+ from filter

--- background.js
+++ background.js
@@ -1,28 +1,6 @@
 let historyItems;
 let fuse;
 
-function searchAndSortResults(searchQuery, items) {
-  const queries = searchQuery
-    .toLowerCase()
-    .split(/[\s,]+/)
-    .filter(Boolean);
-
-  const filteredItems = items
-    .map((item) => {
-      const title = item.title?.toLowerCase() || '';
-      const url = item.url.toLowerCase();
-      const matchCount = queries.reduce((count, query) => {
-        return count + (title.includes(query) || url.includes(query) ? 1 : 0);
-      }, 0);
-      return { item, matchCount };
-    })
-    .filter(({ matchCount }) => matchCount > 0)
-    .sort((a, b) => b.matchCount - a.matchCount)
-    .map(({ item }) => item);
-
-  return filteredItems;
-}
-
 browser.runtime.onMessage.addListener(async (message) => {
   try {
     if (message.action === 'open') {
@@ -61,17 +39,17 @@ browser.runtime.onMessage.addListener(async (message) => {
 
     // console.log('to filter', fused.length);
     // console.time('filter');
-    const filteredItems = fused
-      .filter((_) => _.score < 0.2)
-      .reduce((uniqueItems, item) => {
-        const origin = new URL(item.item.url).origin;
-        if (!uniqueItems.some((i) => new URL(i.url).origin === origin)) {
-          item.item.score = item.score;
-          uniqueItems.push(item.item);
-        }
-        return uniqueItems;
-      }, []);
+    const filteredItems = Array.from(
+      new Map(
+        fused
+          .filter((item) => item.score < 0.2)
+          .map((item) => [new URL(item.item.url).origin, item.item])
+      ).values()
+    );
+
     // console.timeEnd('filter');
+
+    // get the values from filteredItems
 
     // console.time('send');
     // console.log('send', Date.now());
